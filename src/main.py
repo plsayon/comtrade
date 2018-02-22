2c00e9b64af4d3c98ef1c21f7887b8b5650746fc
Filipe Filardi <filipe.jesus@usp.br>
2018-02-22 18:44:20 -0300

    Saving to switch computers.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
+import os
 import requests
+
+DATABASE_DIR = '../database/'
 
 BASE_URL = 'http://comtrade.un.org/api/get?'
 
@@ -11,13 +14,17 @@ TRADE_FLOW = '2' # 2 - export 1 - import
 CLASSIFICATION_CODE = 'TOTAL'
 PARAMS = 'max=50000&type=C&freq=A&px=HS&ps={}&r={}&p={}&rg={}&cc={}&fmt=csv'.format(PERIODS, REPORTERS, PARTNERS, TRADE_FLOW, CLASSIFICATION_CODE)
 
+def check_dependencies():
+	if not os.path.exists(DATABASE_DIR):
+	    os.makedirs(DATABASE_DIR)
+
 def main():
 	response = requests.get(BASE_URL + PARAMS)
-	# .decode('utf-8')
 	
-	file = open("response.csv","w")
+	file = open(DATABASE_DIR + "response.csv","w")
 	file.write(response.content)
 	file.close() 
 	
 if __name__ == '__main__':
+	check_dependencies()
 	main()
